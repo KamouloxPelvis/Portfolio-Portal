4326afd4802a16cc004e4ac9f404b1ee98141c8b
Kamal Guidadou <kamal.guidadou@gmail.com>
2026-03-02 01:13:38 +0100

    Added youtube video link for kguard pitch demo

--- app/components/ProjectModal.tsx
+++ app/components/ProjectModal.tsx
@@ -78,7 +78,12 @@ export default function ProjectModal({ isOpen, onClose, project }: ProjectModalP
             <div className="lg:col-span-3 space-y-8">
               {project.videoPitch && (
                 <div className="aspect-video w-full border border-white/10 bg-black shadow-2xl relative">
-                  <iframe src={project.videoPitch} className="w-full h-full" allowFullScreen title="Video Pitch" />
+                  <iframe 
+                  src={project.videoPitch} 
+                  className="w-full h-full" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowFullScreen 
+                />
                 </div>
               )}
 
